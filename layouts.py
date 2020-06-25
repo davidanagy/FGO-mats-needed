338b572e82acc0e49d3f95216af10dc7d74ff75e
davidanagy <davidanagy@users.noreply.github.com>
2020-06-25 16:43:58 -0700

    finished version 1.0 and deployed to Heroku

--- layouts.py
+++ layouts.py
@@ -14,7 +14,7 @@ asc_options = [
     {'label': 'Ascension MAX', 'value': 5}
 ]
 
-skl_options = [{'label': f'Level {n}', 'value': n} for n in range(1,11)]
+# skl_options = [{'label': f'Level {n}', 'value': n} for n in range(1,11)]
 
 pri_options = [
     {'label': 'Max everything', 'value': 1},
@@ -24,17 +24,45 @@ pri_options = [
 
 # https://dash.plotly.com/dash-core-components/dropdown
 
+row1 = dbc.Row(
+    [
+        dcc.Markdown(
+            """
+            Welcome to FGO Mats Needed! You can use this app to automatically
+            calculate how many materials you'll need to upgrade your Servants
+            to the levels you want.
+
+            To start, use the tools on the left to enter the relevant information
+            for all your Servants (though you can skip any fully-upgraded Servants,
+            since they don't need additional mats anwyway). Alternately, you can
+            upload a CSV or Excel file from a previous session (or one suitably
+            modified that still keeps the same column format).
+
+            If you make a mistake inputting your Servant's data, just re-input it
+            and the old data will be overwritten.
+            """
+        )
+    ]
+)
+
 column1 = dbc.Col(
     [
         dcc.Dropdown(id='servant-selector', placeholder='Search for your Servant'),
         dcc.Dropdown(id='ascension-selector', options=asc_options,
                      placeholder='Select ascension level', searchable=False),
-        dcc.Dropdown(id='skill-selector-1', options=skl_options,
-                     placeholder='First skill level', searchable=False),
-        dcc.Dropdown(id='skill-selector-2', options=skl_options,
-                     placeholder='Second skill level', searchable=False),
-        dcc.Dropdown(id='skill-selector-3', options=skl_options,
-                     placeholder='Third skill level', searchable=False),
+        dcc.Markdown('Input skill levels:'),
+        dcc.Input(id='skill-selector-1', type='number', placeholder='Skill 1',
+                  min=1, max=10, step=1),
+        dcc.Input(id='skill-selector-2', type='number', placeholder='Skill 2',
+                  min=1, max=10, step=1),
+        dcc.Input(id='skill-selector-3', type='number', placeholder='Skill 3',
+                  min=1, max=10, step=1),
+        # dcc.Dropdown(id='skill-selector-1', options=skl_options,
+        #              placeholder='First skill level', searchable=False),
+        # dcc.Dropdown(id='skill-selector-2', options=skl_options,
+        #              placeholder='Second skill level', searchable=False),
+        # dcc.Dropdown(id='skill-selector-3', options=skl_options,
+        #              placeholder='Third skill level', searchable=False),
         dcc.Dropdown(id='priority-selector', options=pri_options,
                      placeholder='Upgrade priority', searchable=False),
         html.Button(id='submit-servant-button', n_clicks=0, children='Select'),
@@ -84,7 +112,9 @@ column2 = dbc.Col(
     ]
 )
 
-index_layout = dbc.Row([column1, column2])
+row2 = dbc.Row([column1, column2])
+
+index_layout = dbc.Col([row1, row2])
 
 ### Mats page layout
 
@@ -108,6 +138,13 @@ table = dbc.Table(table_header + table_body, id='mats-table', bordered=True)
 
 column3 = dbc.Col(
     [
+        dcc.Markdown(
+            """
+            Now, input the amount of each material you currently have.
+            Note that if you leave a field blank, or type something that
+            isn't a number, it will count as 0.
+            """
+        ),
         table,
         dbc.Button('Click here when finished', id='get-final-table', n_clicks=0, color='primary')
     ]
@@ -119,13 +156,39 @@ mats_layout = column3
 
 column4 = dbc.Col(
     [
+        dcc.Markdown(
+            """
+            Click the buttons below to get a CSV copy of the table (so you can
+            save it to e.g. Google Drive) or of your Servants data (so you can
+            upload it to this app again and skip past the first page).
+
+            Scroll down to the bottom for an explanation of the columns.
+            """
+        ),
         dcc.Link(dbc.Button('Get a CSV copy of this table',
                             id='download-mats', n_clicks=0, color='primary'),
                  href='/mats-csv'),
         dcc.Link(dbc.Button('Get a CSV copy of your servant data',
                             id='download-servants', n_clicks=0, color='primary'),
                  href='/servants-csv'),
-        dbc.Table(id='final-table', bordered=True)
+        dbc.Table(id='final-table', bordered=True),
+        dcc.Markdown(
+            """
+            Explanations of columns:
+
+            * "Material": Name of the material
+
+            * "Have": The amount of each material you have, obtained from the previous page
+
+            * "Need": The amount that you need to upgrade all your priority Servants
+
+            * "Want": The amount to fully upgrade all your Servants
+
+            * "Need_Diff": 'Need' minus 'Have'; the table is sorted by this column
+
+            * "Want_Diff": 'Want' minus 'Have'
+            """
+        )
     ]
 )
 
